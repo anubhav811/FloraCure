262ea37063b53638c5744e80a34fcb7f701eae98
Anubhav Chachra <chachraanubhav@gmail.com>
2022-04-20 19:01:24 +0530

    model working , added googling button

--- app/src/main/java/com/anubhav/leafdiseasedetection/ui/activities/MainActivity.kt
+++ app/src/main/java/com/anubhav/leafdiseasedetection/ui/activities/MainActivity.kt
@@ -2,21 +2,47 @@ package com.anubhav.leafdiseasedetection.ui.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import androidx.appcompat.app.ActionBar
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.drawerlayout.widget.DrawerLayout
 import androidx.fragment.app.findFragment
 import androidx.navigation.NavController
 import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
 import com.anubhav.leafdiseasedetection.R
+import com.anubhav.leafdiseasedetection.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+
+    lateinit var  toggle : ActionBarDrawerToggle
+    private lateinit var binding: ActivityMainBinding
 
     lateinit var navController : NavController
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view =  binding.root
+        setContentView(view)
 
-
+        toggle = ActionBarDrawerToggle(this,binding.drawerLayout,0,0)
+        binding.drawerLayout.addDrawerListener(toggle)
+        toggle.syncState()
 //        navController = nav_host_fragment.getFragment<NavHostFragment>().navController
+
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+//        binding.navView.setNavigationItemSelectedListener {
+//            when(it.itemId){
+//                R.id.item1 ->
+//            }
+//        }
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+
+        return super.onOptionsItemSelected(item)
 
     }
 }
--- app/src/main/java/com/anubhav/leafdiseasedetection/ui/fragments/ScanFragment.kt
+++ app/src/main/java/com/anubhav/leafdiseasedetection/ui/fragments/ScanFragment.kt
@@ -1,5 +1,6 @@
 package com.anubhav.leafdiseasedetection.ui.fragments
 
+import android.app.ActionBar
 import android.app.Activity
 import android.content.Context
 import android.content.Intent
